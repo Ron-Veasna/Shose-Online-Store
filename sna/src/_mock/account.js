417718724aa12ec672a7cffa785964e7c629beb3
Ron Veasna <vaing133@gmail.com>
2023-11-22 18:40:09 +0700

    New Update

---
+++ sna/src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Ron Veasna',
+  email: 'Veasna333@gmail.com',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
